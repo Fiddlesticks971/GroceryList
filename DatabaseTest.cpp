1a732bc2434498c3ae2551db159a7bfe747b2b57
Fiddlesticks971 <mitchaheim@gmail.com>
2016-05-17 15:19:09 -0500

    added Test file to test Database Connection

---
+++ DatabaseTest.cpp
@@ -0,0 +1,16 @@
+#include "DatabaseConnection.h"
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(void)
+{
+  DatabaseConnection *TestDB;
+  string filePath;
+  cout << "enter file path: ";
+  cin >> filePath;
+  TestDB = new DatabaseConnection(filePath);
+  cout << TestDB->ConnectToDb();
+  return 0;
+}
